52bdea6e6d47eb504b534b217600509f52193094
Developer <>
2021-10-11 12:38:22 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="nimiqclient",
-    version="0.0.7",
+    version="0.1.0",
     description="A python client for the Nimiq JSON-RPC API",
     long_description=long_description,
     long_description_content_type="text/markdown",
