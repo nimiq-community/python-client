c015d0f53af93d4f4a54c4160d25eca203249c90
Developer <>
2021-10-11 13:18:34 -0400

    fix tests

--- test/session_stub.py
+++ test/session_stub.py
@@ -7,7 +7,7 @@ class SessionStub:
     def post(self, url, json, auth):
         SessionStub.latest_request = json
         SessionStub.latest_request_method = json.get("method")
-        SessionStub.latest_request_params = json.get("params")[0]
+        SessionStub.latest_request_params = json.get("params")
         return self
 
     def json(self):
